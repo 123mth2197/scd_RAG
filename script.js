ddcaf37eb922412348a4caceee94541aeacc9744
Kunal Kumar <79802714+123mth2197@users.noreply.github.com>
2025-12-07 01:51:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 // ------------------------------
 
 // For local dev:
-const API_ASK_URL = "http://localhost:8000/ask";
-const API_RESET_URL = "http://localhost:8000/reset";
+//const API_ASK_URL = "http://localhost:8000/ask";
+//const API_RESET_URL = "http://localhost:8000/reset";
 
 // If you expose via ngrok, just replace the base URL above, e.g.:
-// const API_ASK_URL = "https://your-ngrok-url.ngrok-free.app/ask";
-//const API_URL = "https://nonnegligible-dovelike-emiko.ngrok-free.dev/ask";
+const API_ASK_URL = "https://your-ngrok-url.ngrok-free.app/ask";
+const API_RESET_URL = "https://nonnegligible-dovelike-emiko.ngrok-free.dev/ask";
 
 
 // ------------------------------
@@ -174,3 +174,4 @@ questionInput.addEventListener("keydown", (e) => {
     askQuestion();
   }
 });
+
